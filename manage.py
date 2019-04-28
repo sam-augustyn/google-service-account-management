c4f0c1bb472dbe7faf0736458f37ae86e2334e39
sam-augustyn <25727738+sam-augustyn@users.noreply.github.com>
2019-04-28 18:00:08 -0400

    added comments

--- manage.py
+++ manage.py
@@ -20,36 +20,85 @@ def authenticate(apiName, apiVersion, apiScope):
     return service
 
 def createServiceAccount(service, projectName, userName):
+    ''' This method creates a service account in the specified project with the specified name
+    and returns the service account that was created
+    @service, authenitcated service object
+    @projectName should be the name of the project
+    @userName should be the name of the new service account '''
+
+    #request body for the create account method
     request_body = {"serviceAccount": {"displayName": userName,},"accountId": userName}
+    #return the newly created service account
     return service.projects().serviceAccounts().create(name=projectName, body=request_body).execute()
 
 def getAllAccounts(service, projectName):
+    ''' This gets all the service accounts associated with a projects
+    It returns a dictionary of all the accounts
+    @service, authenticated service object
+    @projectName should be the name of the project '''
+
+    #return all the accounts and their information in a dictionary
     return service.projects().serviceAccounts().list(name=projectName).execute()
 
 def getServiceAccount(service, projectName, userName):
+    ''' Gets details about a service account and returns info about the service account
+    @service is the authenticated service account
+    @projectName should be the name of the project
+    @userName should be the name of the service account'''
+
+    #return the service account information
     return service.projects().serviceAccounts().get(name=generateFullEmail(projectName, userName)).execute()
 
 def generateServiceKey(service, serviceAccount):
+    ''' Generate a private key for a service account and writes json in the current working directory
+    @service, authenticated service object
+    @serviceAccount should the service account info generated from the above methods '''
+
+    #call the api to generate the service account key
     response = service.projects().serviceAccounts().keys().create(name=serviceAccount['name'],
     body={"privateKeyType":"TYPE_GOOGLE_CREDENTIALS_FILE", "keyAlgorithm":"KEY_ALG_RSA_2048"}).execute()
+    #open a file named <serviceAccount>.json in binary mode
     outfile = open(f"{serviceAccount['displayName']}.json", 'wb')
+    #decode the response and write it to a file
     outfile.write(base64.b64decode(response['privateKeyData']))
+    #close the file
     outfile.close()
 
 def listServiceKey(service, userName, name, projectName):
+    '''   '''
     return service.projects().serviceAccounts().keys().list(name=name +
     '/serviceAccounts/' + userName + '@' + projectName + ".iam.gserviceaccount.com").execute()
 
-def removeServiceAccount(service, projectName, userName):
+def removeServiceAccount(service, projectName. userName):
+    ''' Removes the specified service account
+    @service is the authenticated service account
+    @projectName should be the name of the project
+    @userName should be the name of the service account '''
+
+    #call the api to delete the specified service account
     service.projects().serviceAccounts().delete(name=generateFullEmail(projectName, userName)).execute()
 
 def generateServiceEmail(projectName, userName):
+    ''' Generates the service email with the specified service account
+    @projectName should be the name of the project
+    @userName should be the name of the service account '''
+
+    #return a string with the email of the service account
     return userName + "@" + projectName + ".iam.gserviceaccount.com"
 
 def generateProjectName(projectName):
+    ''' Generate the project name
+    @projectName should be the name of the project '''
+
+    #return a string that is the project name
     return "projects/" + projectName
 
 def generateFullEmail(projectName, userName):
+    ''' Generates the full email used for api calls
+    @projectName should be the name of the project
+    @userName should be the name of the service account '''
+
+    #return the string with the full email used for api calls
     return generateProjectName(projectName) + '/serviceAccounts/' + generateServiceEmail(projectName, userName)
 
 def main():
